e3bd8ba718d00d6deebf7d5dc0dffb818f3cc5c5
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-12-29 17:41:09 -0500

    userefrefhooksreact/
    userefrefhooksreact/

---
+++ Box.js
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+
+
+function Box() {
+  const elementRef = useRef();
+
+  function handleMeasureClick() {
+    const div = elementRef.current;
+    console.log("Measurements: ", div.getBoundingClientRect());
+  }
+
+  return (
+    <div ref={elementRef}>
+      <h1>Box</h1>
+      <button onClick={handleMeasureClick}>Measure</button>
+    </div>
+  );
+}
+
+export default Box;
